cc93179f1257697940b035be9bf0617c26a65ab3
YaoKhai <ngeyaokhai@gmail.com>
2026-09-11 22:14:59 +0800

    add reporting

--- auditor.py
+++ auditor.py
@@ -1,10 +1,13 @@
 inventory = 0
+rejected_ent = 0
 
 while True:
     stockQuantity = input("Enter a stock quantity: ")
     if not stockQuantity.isdigit() or int(stockQuantity) < 0:
         if stockQuantity.lower().strip() == "quit":
+            print(f"Total Units Processed: {inventory}\nNumber of Failed/Rejected Entries: {rejected_ent}")
             break
+        rejected_ent += 1
         print("Invalid input, please try again!")
         continue
     inventory += int(stockQuantity)
