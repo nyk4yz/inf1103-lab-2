8ecee70835d5764c039a37ea19bf272ee29f0192
YaoKhai <ngeyaokhai@gmail.com>
2026-09-11 22:03:05 +0800

    continuous loop until user types quit

--- auditor.py
+++ auditor.py
@@ -1,1 +1,7 @@
 inventory = 0
+
+while True:
+    stockQuantity = input("Enter a stock quantity: ")
+    if not stockQuantity.isdigit() or int(stockQuantity) < 0:
+        if stockQuantity.lower().strip() == "quit":
+            break
